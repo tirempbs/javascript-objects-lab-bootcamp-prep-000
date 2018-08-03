1453e672710f010f96020abab3c0f0f64fc76459
Tirem Bareno-Sosna <tirempbs@gmail.com>
2018-08-03 17:14:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-  delete object[key];
-  return object;
+  var newObject = Object.assign({}, object);
+  delete newObject.key;
+  return newObject;
 }
